ac01dfad8633d25284f98612a1da2c9a74fef089
john.mukenge <john.mukenge@emeal.nttdata.com>
2025-01-19 09:27:39 +0100

    header component [work in progress]

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <header className="header bg-cover bg-center h-64">
+        <div className="flex items-center justify-center h-full">
+          <h1 className="text-white text-4xl">Avventisti del Settimo Giorno Movimento di Riforma</h1>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default Header
